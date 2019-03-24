c6bacc7e18522f5189c857bd7f1d48d4f3ac21fa
Liliya Sheremeta <liljsik1@gmail.com>
2019-03-24 19:38:00 +0200

    Home work array

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var arr = ['a', 'b', 'c'];
+var arr1 = [1, 2, 3];
+var end = arr.concat(arr1);
+console.log(end);
+
+var arr2 = ['a', 'b', 'c'];
+arr2.push(1, 2, 3);
+console.log(arr2);
+
+var number = [1, 2, 3];
+number.reverse();
+console.log(number);
+
+var arr3 = [1, 2, 3];
+arr3.unshift(4, 5, 6);
+console.log(arr3);
+
+var arr4 = [1, 2, 3, 4, 5];
+arr4.splice(1, 2);
+console.log(arr4);
+
